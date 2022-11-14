d5329b6f9f3984ee255f749894bc02a8cd4ee75d
jykwon <jykwon@12cm.co.kr>
2022-11-14 21:33:04 +0900

    스프링으로 전환하기

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,6 +9,8 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // 어플리케이션의 전체 동작 방식을 구성(config)하기 위해,
 // 구현 객체를 생성하고, 연결하는 책임을 가지는 별도의 설정 클래스
@@ -16,21 +18,25 @@ import hello.core.order.OrderServiceImpl;
 // AppConfig는 어플리케이션의 실제 동작에 필요한 구현 객체를 생성한다.`
 
 // AppConfig는 생성한 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입(연결)한다.
-
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
