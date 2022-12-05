5e6b6b7bf960a4f6eb56dbcbbde1df7d656880fb
jykwon <jykwon@12cm.co.kr>
2022-12-05 21:02:18 +0900

    롬복과 최신 트랜드

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,17 +4,19 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+
+@Component
+// final이 붙은 필드를 모아서 생성자를 자동으로 만들어 준다.
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     // final 이면 기본으로 할당을 시켜주거나, 생성자가 있어야 함.
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
