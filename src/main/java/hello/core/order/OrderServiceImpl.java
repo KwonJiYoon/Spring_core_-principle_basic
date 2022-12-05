87c37d04ce6c54ba133dec6eb7f632208e1b37e9
jykwon <jykwon@12cm.co.kr>
2022-12-05 21:07:32 +0900

    조회 빈이 2개 이상 - 문제 발생

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -11,12 +11,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 // final이 붙은 필드를 모아서 생성자를 자동으로 만들어 준다.
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     // final 이면 기본으로 할당을 시켜주거나, 생성자가 있어야 함.
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
